f462986918b16f8d739a916d3820d349eb99cbd1
Auguspo <augustpolet@gmail.com>
2024-10-24 14:36:16 -0300

    GET & Read Methods [PART 3]

---
+++ Interfaces/IOwnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int OwnerId);
+        bool OwnerExists(int ownerId);
+    }
+}
