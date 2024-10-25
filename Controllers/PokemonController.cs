d68d2ccffe8a4faf7a1fd64c28780a44d071db6b
Auguspo <augustpolet@gmail.com>
2024-10-25 19:03:47 -0300

    DELETE Methods

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -144,5 +144,29 @@ namespace PokemonReviewApp.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{pokeId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeletePokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId)) { return NotFound(); };
+
+            var reviewsToDelete = _reviewRepository.GetReviewsOfAPokemon(pokeId);
+            var pokeToDelete = _pokemonRepository.GetPokemon(pokeId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewRepository.DeleteReviews(reviewsToDelete.ToList())) { ModelState.AddModelError("", "Something wnet wrong when deleting reviews"); }
+
+            if (!_pokemonRepository.DeletePokemon(pokeToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong deleting pokemon");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
     }
 }
